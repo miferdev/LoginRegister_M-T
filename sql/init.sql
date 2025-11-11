332fe131471411a4e7b22408db4736da4ba5c459
usertar <fdezguerrerom@gmail.com>
2025-11-11 17:51:28 +0100

    creada la base de datos sql

---
+++ sql/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CLIENTE (
+    id_cliente SERIAL PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+    apellido VARCHAR(30) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    telefono VARCHAR(13),
+    direccion_envio VARCHAR(255),
+    ciudad VARCHAR(100),
+    codigo_postal VARCHAR(20) 
+);
