f6869f66e78cc07e0273fd3a2afb8b41f7cad469
usertar <fdezguerrerom@gmail.com>
2025-11-13 17:54:47 +0100

    modificaciones en registercheck

--- backend/registercheck.php
+++ backend/registercheck.php
@@ -2,25 +2,24 @@
     session_start();
     require_once("config.php");
 
-    if (isset($_POST["register"])) {
-        $nombre = $_POST["name"];
-        $contrasenia = password_hash($_POST["password"], PASSWORD_DEFAULT);
-        $correo = $_POST["correo"];
-        $telefono = $_POST["telefono"];
-        $direccion = $_POST["direccion"];
-        $ciudad = $_POST["ciudad"];
-        $codigoPostal = $_POST["CP"];
+    $nombre = $_POST["name"];
+    $contrasenia = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $correo = $_POST["correo"];
+    $telefono = $_POST["telefono"] ?? "";
+    $direccion = $_POST["direccion"] ?? "";
+    $ciudad = $_POST["ciudad"] ?? "";
+    $codigoPostal = $_POST["CP"] ?? "";
 
-        $checkMail = $conn->query("SELECT email FROM CLIENTES WHERE email = '$email'");
-        
-        if ($checkMail->num_rows > 0) {
-        $_SESSION["register_error"] = "Email registrado";
-        $_SESSION["active_form"] = "register";
-        
-        }else {
-            $conn->query("INSERT INTO CLIENTES (nombre, contrasenia, email, telefono, direccion_envio, ciudad, codigo_postal) VALUES ('$nombre', '$contrasenia', '$correo', '$telefono', '$direccion', '$ciudad', '$codigoPostal')");
-        }
-        header("Location: ../frontend/registro.php");
-    exit();
+    $checkMail = $conn->query("SELECT email FROM CLIENTES WHERE email = '$email'");
+    
+    if ($checkMail->num_rows > 0) {
+    $_SESSION["register_error"] = "Email registrado";
+    $_SESSION["active_form"] = "register";
+    
+    }else {
+        $conn->query("INSERT INTO CLIENTES (nombre, contrasenia, email, telefono, direccion_envio, ciudad, codigo_postal) VALUES ('$nombre', '$contrasenia', '$correo', '$telefono', '$direccion', '$ciudad', '$codigoPostal')");
     }
+    header("Location: ../frontend/registro.php");
+    exit();
+    
 ?>
